1d7611838bb6fd34d145bf5d32c5dd42d059d1ae
Fabiano Amaral Freitas <fabianin.amaral@gmail.com>
2014-10-26 17:59:32 -0200

    criado com ordenado e se passar um parametro a mais gera os indices tbm

--- cria2-0.c
+++ cria2-0.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
- 
+#include "lib.h"
 #define BSIZE 100
  
 int main(int argc, char* argv[]){
@@ -27,12 +27,16 @@ int main(int argc, char* argv[]){
 	int comp, i,tamanhoString1,tamanhoString2,count;
 	char *nome, *snome;
 	int x;
-	char buffer[BSIZE];
-	int pos1=0;
+	int offset=0;
+	char buffer[BSIZE],buffer2[BSIZE];
+	int pos1=0, pos2=0;
 	int j;
 	float n1,n2,n3;
-	FILE* arq;
-	arq = fopen("arquivo.txt","w+");
+	FILE* arq, *indices;
+	arq = fopen("dados.bin","wb+");
+	if((argc==4 && *argv[2]=='P') || (*argv[2]!='P')){
+			indices = fopen("indices.bin","wb+");
+		}
 	if(argc <2){
 			exit(1);
 	}
@@ -41,6 +45,7 @@ int main(int argc, char* argv[]){
 	for(j=0;j<qtReg;j++){
 		ra = j;
 		comp=0;
+		comp+=sizeof(int);//RA e comprimento do registro
 		comp+=sizeof(int);
 		//srand(time(NULL));
 		tamanhoString1 = 1 + rand()%18; //sorteia o tamanho do nome
@@ -50,15 +55,17 @@ int main(int argc, char* argv[]){
 			nome[count]=caracter[rand()%25];
 			//printf("%c",nome[count]);
 		}
-		comp+=sizeof(char)*(tamanhoString1+1);
+		nome[count]='\0';
+		comp+=sizeof(char)*(tamanhoString1+1); //O tamanho do Nome + 1 caracter separador de campo
 		tamanhoString2 = 1 + rand()%18; //sorteia o tamanho do sobrenome
 		snome = (char *)(malloc(sizeof(char)*tamanhoString2)); //alone memoria pro sobrenome
 		for(count=0;count<tamanhoString2;count++)
 		{
 				snome[count]=caracter[rand()%25];
 		}
-		comp+=sizeof(char)*(tamanhoString2+1);
-		comp+=sizeof(float)*3;
+		snome[count]='\0';
+		comp+=sizeof(char)*(tamanhoString2+1); //O tamanho do Nome + 1 caracter separador de campo
+		comp+=sizeof(float)*3; //as 3 notas
 		if(BSIZE-pos1 < comp){
 			if(pos1+sizeof(char)<=BSIZE)buffer[pos1] = '*';
 			for (i = pos1; i < BSIZE; i++)
@@ -74,6 +81,10 @@ int main(int argc, char* argv[]){
 		n2 =(float) x/100;
 		x = (rand()%1000);
 		n3 =(float) x/100;
+		//if(j==0){
+			//memcpy(&buffer[pos1],&qtReg,sizeof(int));
+			//pos1+=sizeof(int);
+		//}
 		memcpy(&buffer[pos1],&comp,sizeof(int));
 		pos1+=sizeof(int);
 		memcpy(&buffer[pos1],&ra,sizeof(int));
@@ -86,28 +97,49 @@ int main(int argc, char* argv[]){
 		pos1+=sizeof(char)*(tamanhoString2);
 		buffer[pos1]='#';
 		pos1++;
-		memcpy(&buffer[pos1],&n1,sizeof(int));
+		memcpy(&buffer[pos1],&n1,sizeof(float));
 		pos1+=sizeof(float);
-		memcpy(&buffer[pos1],&n2,sizeof(int));
+		memcpy(&buffer[pos1],&n2,sizeof(float));
 		pos1+=sizeof(float);
-		memcpy(&buffer[pos1],&n3,sizeof(int));
+		memcpy(&buffer[pos1],&n3,sizeof(float));
 		pos1+=sizeof(float);
-		if(argc == 3){
+		if(*argv[2] == 'P'){
 			if(*argv[2]=='P'){
 				printf("%d\t%d\t%-20s\t%-20s\t%.2f\t%.2f\t%.2f\n",comp, ra,nome,snome,n1,n2,n3);
 			}
 		}
-		//nome=NULL;
+		if((argc==4 && *argv[2]=='P') || (*argv[2]!='P')){
+			if(BSIZE-pos2 < 8){
+				if(pos2+sizeof(char)<=BSIZE){
+					buffer2[pos2] = '*';
+				}
+				for (i = pos2; i < BSIZE; i++)
+				{
+					buffer[pos2] = '*';
+				}
+				fwrite(buffer2,BSIZE,1,indices);
+				pos2=0;
+			}
+				memcpy(&buffer2[pos2],&ra,sizeof(int));
+				pos2+=sizeof(int);
+				memcpy(&buffer2[pos2],&offset,sizeof(int));
+				pos2+=sizeof(int);
+				offset+=comp;
+		}
 		free(nome);
-		snome=NULL;
 		free(snome);
-	}
+		}
 		for (i = pos1; i < BSIZE; i++)
 			{
 				buffer[pos1] = '*';
 			}
 		fwrite(buffer,BSIZE,1,arq);
 		pos1=0;
+		if((argc==4 && *argv[2]=='P') || (*argv[2]!='P'))
+		{
+			fclose(indices);
+		}
+		
 		fclose(arq);
 		return 0;
 }
